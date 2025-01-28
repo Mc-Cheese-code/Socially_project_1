268296e11146ab49d155aaf8067ab95a7a878454
Siddhartha <sidd.patty@gmail.com>
2025-01-28 20:49:21 +0530

    sixth commit all done expect deployment

--- src/components/ui/CreatePost.tsx
+++ src/components/ui/CreatePost.tsx
@@ -9,6 +9,7 @@ import { Button } from "./button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./imageUpload";
 
 //since its a client component now async cannot be used 
 
@@ -57,7 +58,18 @@ function CreatePost() {
                 />
                 </div>
 
-
+                {(showImageUpload || imageUrl) && (
+                <div className="border rounded-lg p-4">
+                    <ImageUpload
+                        endpoint="postImage"
+                        value={imageUrl}
+                        onChange={(url) => {
+                        setImageUrl(url);
+                        if (!url) setShowImageUpload(false);
+                        }}
+                    />
+                </div>
+                )}
 
                 <div className="flex items-center justify-between border-t pt-4">
                     <div className="flex space-x-2">
